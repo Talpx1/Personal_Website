73354b9584f20347b3418d11b7d0eb2228399cc2
Talpx1 <simo3401@gmail.com>
2023-11-06 19:06:33 +0100

    maybe fix UI mobile

--- src/views/partials/intro.partial.php
+++ src/views/partials/intro.partial.php
@@ -1,4 +1,4 @@
-<section class="grid place-content-center text-dark dark:text-light  bg-light dark:bg-dark font-primary min-h-screen place-items-center gap-8 fixed top-0 left-0 z-40 w-full border-8 border-dark dark:border-light border-solid" id="intro">
+<section class="grid place-content-center text-dark dark:text-light  bg-light dark:bg-dark font-primary place-items-center gap-8 fixed top-0 left-0 z-40 w-full border-8 border-dark dark:border-light border-solid" id="intro">
     <svg class="w-3/4 lg:w-full h-auto" width="96" height="51" viewBox="0 0 96 51" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path class="stroke-dark dark:stroke-light" d="M1 49.2494C22 49.2494 24.0781 42.1243 34.5 27.7493C49 7.7493 72 -8.7506 95 7.74931C72 7.74931 72 26.2492 72 27.7492C72 29.2492 72 49.2492 95 49.2492" stroke="#FDFFFC" stroke-width="2" stroke-linecap="square" stroke-linejoin="bevel"/>
     </svg>
