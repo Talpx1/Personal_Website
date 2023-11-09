403647ce58b02b11cfb1fe8edd2c2e972b74ba35
Talpx1 <simo3401@gmail.com>
2023-11-09 20:57:47 +0100

    maybe fix bg noise

--- src/views/home.view.php
+++ src/views/home.view.php
@@ -12,8 +12,8 @@
     <main class="text-center flex flex-col content-center items-center justify-center grow">
         <div class="flex flex-col content-center gap-28 grow justify-center">
             <div class="flex flex-col justify-center gap-5">
-                <h1 class="font-bold whitespace-nowrap text-2xl lg:text-4xl tracking-widest"><?= t("hi_im_simone", '<span class="font-accent text-4xl lg:text-6xl rainbow-underline px-1">') ?> :)</h1>
-                <h2 class="font-bold whitespace-nowrap text-xl lg:text-2xl tracking-widest"><?= t('a_fullstack_developer', '<span class="font-accent text-2xl lg:text-4xl rainbow-underline px-1">') ?></h2>
+                <h1 class="font-bold whitespace-nowrap text-2xl lg:text-4xl tracking-widest"><?= t("hi_im_simone", '<span class="whitespace-nowrap font-accent text-4xl lg:text-6xl rainbow-underline px-1">') ?> :)</h1>
+                <h2 class="font-bold whitespace-nowrap text-xl lg:text-2xl tracking-widest"><?= t('a_fullstack_developer', '<span class="whitespace-nowrap font-accent text-2xl lg:text-4xl rainbow-underline px-1">') ?></h2>
             </div>
             <nav class="flex flex-row w-full md:w-2/3 lg:w-fit justify-between md:justify-evenly lg:gap-52 content-center text-l lg:text-xl">
                 <a class="font-bold relative transition-all after:transition-all after:h-1 after:w-0 after:duration-300 after:block after:absolute after:bottom-0 after:left-0 hover:after:w-full dark:hover:after:bg-light hover:after:bg-dark" href="/<?= app()->locale ?>/projects"><?= t('projects') ?></a>
