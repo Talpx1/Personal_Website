e4b2f2085e234a45c632dc0a0103e60431e0ea2c
Talpx1 <simo3401@gmail.com>
2024-03-07 22:30:53 +0100

    used Safe\parse_url in DetectLocale middleware

--- src/Http/Middlewares/DetectLocale.php
+++ src/Http/Middlewares/DetectLocale.php
@@ -3,11 +3,11 @@
 namespace SC\Http\Middlewares;
 
 use SC\Http\Middlewares\Contracts\Middleware;
+use function Safe\parse_url;
 
 class DetectLocale implements Middleware {
-
     public function handle(): void {
-        $uri_path = parse_url($_SERVER['REQUEST_URI'])['path'];
+        $uri_path = parse_url($_SERVER['REQUEST_URI'])['path'] ?? "/";
         $locale = explode("/", $uri_path)[1];
         
         app()->setLocale($locale);
